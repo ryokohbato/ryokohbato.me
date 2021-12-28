e1c1ac9c61e386ec060cb3d75fa5eae328682614
ryokohbato <ryokohbato@gmail.com>
2021-12-28 23:14:57 +0900

    🔧 プロフィール画像とリンクを表示

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,1 +1,22 @@
 import "../style/style.scss";
+
+const profileImageElement: HTMLElement = document.querySelector(".profile")!;
+
+profileImageElement.addEventListener("click", () => {
+  console.log("loaded");
+  startAnimation();
+})
+
+const startAnimation = () => {
+  if (profileImageElement.classList.contains("rotate")) {
+    profileImageElement.classList.remove("rotate");
+  }
+  setTimeout(() => {
+    profileImageElement.classList.add("rotate");
+  }, 100);
+}
+
+window.addEventListener("load", () => {
+  console.log("loaded");
+  startAnimation();
+})
