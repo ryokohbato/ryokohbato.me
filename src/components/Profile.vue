cc3b5e91dfab2b404e18c9284057d05c0de88fba
ryokohbato <ryokohbato@gmail.com>
2022-02-04 04:26:56 +0900

    変更忘れを修正

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -9,7 +9,7 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class HelloWorld extends Vue {
+export default class Profile extends Vue {
   @Prop() private name!: string;
 }
 </script>
